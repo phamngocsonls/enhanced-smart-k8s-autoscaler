ecfee882f48215064b37250c25e43df2fb2f9fdf
json <phamngocsonls@gmail.com>
2025-12-31 00:57:25 +0700

    fix: update test version to 0.0.8v2

--- tests/test_core_features.py
+++ tests/test_core_features.py
@@ -220,9 +220,9 @@ class TestVersioning:
         assert parts[2][0].isdigit()
     
     def test_version_value(self):
-        """Test version is 0.0.8v1"""
+        """Test version is 0.0.8v2"""
         import src
-        assert src.__version__ == "0.0.8v1"
+        assert src.__version__ == "0.0.8v2"
 
 
 if __name__ == "__main__":
