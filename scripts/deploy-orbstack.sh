1cd7d30f5f6334c7187a3071553c1d4aab462608
json <phamngocsonls@gmail.com>
2026-01-10 20:50:42 +0700

    v0.0.34-v2: Notification providers, alert subscriptions, pre-scale fix, webhook testing

--- scripts/deploy-orbstack.sh
+++ scripts/deploy-orbstack.sh
@@ -298,7 +298,7 @@ spec:
       serviceAccountName: smart-autoscaler
       containers:
       - name: operator
-        image: ghcr.io/phamngocsonls/enhanced-smart-k8s-autoscaler:0.0.34
+        image: ghcr.io/phamngocsonls/enhanced-smart-k8s-autoscaler:0.0.34-v2
         imagePullPolicy: IfNotPresent
         envFrom:
         - configMapRef:
